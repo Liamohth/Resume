730c2eda359e5ae3e0d610c6aaa2e11431f3e0fb
Liamohth <45179229+Liamohth@users.noreply.github.com>
2020-06-05 17:16:58 -0700

    Add files via upload

---
+++ js/style.js
@@ -0,0 +1,7 @@
+document.querySelector("#technical-skills").addEventListener("mouseover", function(){
+ document.querySelector("#technical-skills").classList.add("technical-skills-js");
+});
+
+document.querySelector("#technical-skills").addEventListener("mouseover", function(){
+document.querySelector(".technical-skills").className ="technical-skills-list-2";
+});
